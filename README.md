ca26630e19dd8516b223f4f40b3b867ae5cb1848
Bentley Turner <bentleyturner@live.com>
2020-02-27 20:26:22 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# TripCalculatorCodingChallenge" 
+# TripCalculatorCodingChallenge
+# TripCalculatorCodingChallenge
---
+++ fareCalculator/src/test/java/com/bentley/farecalculator/fareCalculator/FareCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bentley.farecalculator.fareCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FareCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
