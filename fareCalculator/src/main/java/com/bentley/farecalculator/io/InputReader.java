67057cdab8cc7269d892ce17d1de74584ddb409b
Bentley Turner <bentleyturner@live.com>
2020-02-29 03:23:53 +1100

    Added inputReader and OutputWriter

--- fareCalculator/src/main/java/com/bentley/farecalculator/io/InputReader.java
+++ fareCalculator/src/main/java/com/bentley/farecalculator/io/InputReader.java
@@ -1,5 +1,27 @@
 package com.bentley.farecalculator.io;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+
+
+@Component
+@AllArgsConstructor
 public class InputReader{
+
+    private final CsvMapper csvMapper;
+
+    public <T> Iterator<T> parse(String file, Class<T> pClass) throws IOException{
+
+        File inputFile = new File(file);
+        return csvMapper.readerFor(pClass).with(CsvSchema.emptySchema().withHeader()).readValues(file);
+    }
 
 }
--- fareCalculator/src/main/java/com/bentley/farecalculator/io/OutputWriter.java
+++ fareCalculator/src/main/java/com/bentley/farecalculator/io/OutputWriter.java
@@ -1,5 +1,40 @@
 package com.bentley.farecalculator.io;
 
+import java.io.IOException;
+import java.io.File;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@AllArgsConstructor
 public class OutputWriter{
+    private final CsvMapper csvMapper;
+
+    public <T> void createOutputFile(String filename, List<T> data, Class<T> pClass) throws IOException{
+        File outputFile = new File(filename);
+        outputFile.getParentFile().mkdirs();
+        FileOutputStream outStream = new FileOutputStream(outputFile);
+        BufferedOutputStream bufferedOutStream = new BufferedOutputStream(outStream);
+        OutputStreamWriter outStreamWriter = new OutputStreamWriter(bufferedOutStream, StandardCharsets.UTF_8);
+
+        //CsvSchema pClassSchema = csvMapper.schemaFor(pClass);
+        CsvSchema pClassSchema = csvMapper.schemaFor(pClass).withHeader().withoutQuoteChar();
+        ObjectWriter pClassObjectWriter = csvMapper.writer(pClassSchema);
+        pClassObjectWriter.writeValue(outStreamWriter, data);
+
+    }
 
 }
